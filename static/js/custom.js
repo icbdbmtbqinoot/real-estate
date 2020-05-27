7436ae43914df40e366500b2790438504fe73fdd
Miso <miso4844@gmail.com>
2020-05-27 19:41:49 +0900

    index page editing

---
+++ static/js/custom.js
@@ -0,0 +1,6 @@
+   // const MDCDataTable = mdc.dataTable.MDCDataTable;
+    // const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
+    //const MDCFoo = mdc.foo.MDCFoo;
+    // const MDCFooFoundation = mdc.foo.MDCFooFoundation;
+    // const foo = new MDCFoo(document.querySelector('.mdc-foo'));
+    //mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
