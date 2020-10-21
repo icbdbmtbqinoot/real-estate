670400b57b0ad58ad3b09fdac25f568087b39b06
Miso <miso4844@gmail.com>
2020-10-21 18:46:06 +0900

    modify designs

--- realtorproject/settings.py
+++ realtorproject/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'realtorproject/static')
 ]
+
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- static/js/custom.js
+++
@@ -1,6 +0,0 @@
-   // const MDCDataTable = mdc.dataTable.MDCDataTable;
-    // const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
-    //const MDCFoo = mdc.foo.MDCFoo;
-    // const MDCFooFoundation = mdc.foo.MDCFooFoundation;
-    // const foo = new MDCFoo(document.querySelector('.mdc-foo'));
-    //mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
